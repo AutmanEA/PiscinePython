21bc6b1bf87768650c34c2cdc036bfee951a2e25
Autman EL ATMIOUI <autman.elatmioui@gmail.com>
2026-01-28 16:42:28 +0100

    ADD: ex05 finished and ex06 done

--- 0_Starting/ex05/building.py
+++ 0_Starting/ex05/building.py
@@ -13,8 +13,26 @@ def countStr(s: str):
     :param s: Text to count
     :type s: str
     """
-    print(f"The text contains {len(s)} characters:")
-    print(s)
+    counters = {"upper": 0, "lower": 0, "punct": 0, "digit": 0, "spaces": 0}
+    print(f"\nThe text contains {len(s)} characters:")
+    for char in s:
+        if (char.islower()):
+            counters["lower"] += 1
+        elif (char.isupper()):
+            counters["upper"] += 1
+        elif (char.isspace()):
+            counters["spaces"] += 1
+        elif (char.isdigit()):
+            counters["digit"] += 1
+        elif (char.isprintable()):
+            counters["punct"] += 1
+    print(f"""
+    + {counters["upper"]} upper letters
+    + {counters["lower"]} lower letters
+    + {counters["punct"]} punctuation marks
+    + {counters["spaces"]} spaces
+    + {counters["digit"]} digits
+    """)
 
 
 def main():
@@ -28,16 +46,15 @@ def main():
         assert len(sys.argv) <= 2, "AssertionError: too many arguments"
         match len(sys.argv):
             case 1:
-                print("What is the text to count ?")
-                str = input()
+                str = sys.stdin.readline()
             case 2:
                 str = sys.argv[1]
         countStr(str)
     except AssertionError as e:
         print(e)
-    except EOFError:
-        pass
-    except:
+    except KeyboardInterrupt:
+        print("\nInterrupted")
+    except Exception:
         print("Fatal error: something went wrong")
 
 
---
+++ 0_Starting/ex06/filterstring.py
@@ -0,0 +1,40 @@
+import sys
+from ft_filter import ft_filter
+
+
+def isStringValid(string: str) -> bool:
+    """
+    returns True if param string contains only
+    alphanumeric or space characters
+    """
+    for char in string:
+        if char.isalnum() or char.isspace():
+            pass
+        else:
+            return False
+    return True
+
+
+def main():
+    """
+    Filterstring takes a string and show all
+    words bigger than len limit specified in 2nd argument
+    """
+    try:
+        assert len(sys.argv) == 3, "AssertionError: bad arguments"
+        assert isStringValid(sys.argv[1]), "AssertionError: bad arguments"
+        assert sys.argv[2].isnumeric(), "AssertionError: bad arguments"
+        wordlist = sys.argv[1].split()
+        minlen = int(sys.argv[2])
+        newlist = list(ft_filter(lambda x: len(x) > minlen, wordlist))
+        print(newlist)
+    except AssertionError as e:
+        print(e)
+    except KeyboardInterrupt:
+        print("\nInterrupted")
+    except Exception:
+        print("Fatal error: something went wrong")
+
+
+if __name__ == "__main__":
+    main()
