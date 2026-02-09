02485b15d9257080ff9eff530878da45f66041b8
Autman EL ATMIOUI <autman.elatmioui@gmail.com>
2026-02-09 13:44:32 +0100

    UPDATE: ex02 done, image loader that prints all rgb pixels

--- 1_Array/ex02/load_image.py
+++ 1_Array/ex02/load_image.py
@@ -1,9 +1,17 @@
-def main():
-    """
-    Docstring for main
-    """
-    pass
+import numpy as np
+import matplotlib.image as mpimg
 
 
-if __name__ == "__main__":
-    main()
+def ft_load(path: str) -> np.array:
+    """
+    ft_load loads an image and displays all pixels RGB values
+    - raises Exception on bad image
+    """
+    if (not isinstance(path, str)):
+        raise Exception("Bad path format for image")
+    try:
+        img = mpimg.imread(path)
+    except Exception:
+        raise Exception("No file to read or not an image")
+    print('The shape of image is :', img.shape)
+    return img
