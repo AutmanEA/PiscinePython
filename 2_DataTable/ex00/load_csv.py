67b258d4b9891f5eaef365d106431b4c3f74231a
Autman EL ATMIOUI <autman.elatmioui@gmail.com>
2026-02-10 13:01:53 +0100

    ADD: ex00 done

--- 2_DataTable/ex00/load_csv.py
+++ 2_DataTable/ex00/load_csv.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
 
-def load_csv(path: str):
+def load_csv(path: str) -> pd.DataFrame | None:
     """
+    Loads CSV file from path and displays its dimensions
+    - returns None on path error
     """
-    pass
+    if not isinstance(path, str):
+        return None
+    try:
+        dataset = pd.read_csv(path)
+    except Exception:
+        return None
+    print("Loading dataset of dimensions", dataset.shape)
+    pd.options.display.show_dimensions=False
+    pd.options.display.max_columns=8
+    return(dataset)
