ac7e639695ca6cf792e12c22640b175f2c304d54
Autman EL ATMIOUI <autman.elatmioui@gmail.com>
2026-02-12 09:31:06 +0100

    UPDATE: ex01 added error handling and doc

--- 2_DataTable/ex01/aff_life.py
+++ 2_DataTable/ex01/aff_life.py
@@ -5,17 +5,28 @@ from load_csv import load_csv
 
 def main():
     """
+    This programs loads data life expectancy years
+    and displays France plot based on this data
+    - France life expectancy over time
     """
     filename = "life_expectancy_years.csv"
     path = f"/home/ael-atmi/Cursus/PicPython/2_DataTable/res/{filename}"
-    df = pd.DataFrame(load_csv(path))
-    df_france = df[df['country']=='France'].T[1:]
-    df_france.plot(kind='line',
-                   title='France life expectancy projection',
-                   xlabel='Year',
-                   ylabel='Life Expectancy',
-                   legend=None)
-    plt.show()
+    data = load_csv(path)
+    if data is None:
+        print("Error: path not valid.")
+    try:
+        df = pd.DataFrame(data)
+        df_france = df[df['country']=='France'].T[1:]
+        df_france.plot(kind='line',
+                    title='France life expectancy projection',
+                    xlabel='Year',
+                    ylabel='Life Expectancy',
+                    legend=None)
+        plt.show()
+    except Exception as e:
+        print(e)
+    except KeyboardInterrupt:
+        print("Program interupted manualy")
 
 
 if __name__ == "__main__":
