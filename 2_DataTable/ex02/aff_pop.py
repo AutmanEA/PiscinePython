0efe5ffd89a8cd93624af702ef8afb14309034d4
Autman EL ATMIOUI <autman.elatmioui@gmail.com>
2026-02-12 14:16:27 +0100

    UPDATE: ex02 error handling

--- 2_DataTable/ex02/aff_pop.py
+++ 2_DataTable/ex02/aff_pop.py
@@ -25,27 +25,27 @@ def main():
     data = load_csv(path)
     if data is None:
         print("Error: path not valid.")
-    # try:
-    df_pop = pd.DataFrame(data.set_index('country'), dtype='str')
-    df_pop = df_pop.loc[['France', 'Japan'], '1800':'2050'].map(convert_data_numbers)
-    maxpop = int(df_pop.max().max())
-    minpop = int(df_pop.min().min())
-    df_pop.T.plot(title='Population projections',
-                xlabel='Year',
-                ylabel='Population',
-                )
-    yticks = range(minpop - 10000000, maxpop + 10000000, 20000000)
-    ylabels = [f'{x // 1000000}M' for x in yticks]
-    plt.yticks(yticks, ylabels)
-    xticks = range(0,2050 - 1800,40)
-    xlabels = [f'{x + 1800}' for x in xticks]
-    plt.xticks(xticks, xlabels)
-    plt.legend(loc='upper left')
-    plt.show()
-    # except Exception as e:
-        # print(e)
-    # except KeyboardInterrupt:
-        # print("Program interupted manualy")
+    try:
+        df_pop = pd.DataFrame(data.set_index('country'), dtype='str')
+        df_pop = df_pop.loc[['France', 'Japan'], '1800':'2050'].map(convert_data_numbers)
+        maxpop = int(df_pop.max().max())
+        minpop = int(df_pop.min().min())
+        df_pop.T.plot(title='Population projections',
+                    xlabel='Year',
+                    ylabel='Population',
+                    )
+        yticks = range(minpop - 10000000, maxpop + 10000000, 20000000)
+        ylabels = [f'{x // 1000000}M' for x in yticks]
+        plt.yticks(yticks, ylabels)
+        xticks = range(0,2050 - 1800,40)
+        xlabels = [f'{x + 1800}' for x in xticks]
+        plt.xticks(xticks, xlabels)
+        plt.legend(loc='upper left')
+        plt.show()
+    except Exception as e:
+        print(e)
+    except KeyboardInterrupt:
+        print("Program interupted manualy")
 
 
 if __name__ == "__main__":
